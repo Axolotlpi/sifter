1154bec88284ce1cd8e2dd3c1c1aea72c693f7bb
Axolotlpi <cfc3.1415@gmail.com>
2022-07-03 17:06:52 -0500

    add web manifest integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,21 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+
+import webmanifest from 'astro-webmanifest';
+import webmanifestConfig from './webmanifest.config.js';
 
 // https://astro.build/config
 export default defineConfig({
 	// site: 'example.com',
+	experimental: {
+		integrations: true
+	},
 	integrations: [
 		tailwind({
-			config: { applyBaseStyles: false }
-		})
+			config: {
+				applyBaseStyles: false
+			}
+		}),
+		webmanifest(webmanifestConfig)
 	]
 });
---
+++ webmanifest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	icon: 'src/assets/icon.svg', // source for favicon & icons
+	name: 'Sifter', // required
+	short_name: 'Sifter',
+	description: 'Sift through text',
+	start_url: '/',
+	theme_color: '#0090DE',
+	background_color: '#353D40',
+	display: 'standalone'
+};
