bd9fe44b43d55a62c9e92fe3f25a50b92a2db6e2
Caleb Favela <cfc3.1415@gmail.com>
2022-07-09 21:05:23 -0500

    add and test svelte integration

---
+++ src/components/Counter.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    let count = 0;
+</script>
+
+<h2>Count: {count}</h2>
+<button on:click={() => count++}>+</button>
+<button on:click={() => count--}>-</button>
+
+<style>
+    button {
+        margin: 0.5em;
+        border: 2px solid orangered;
+        font-weight: bold;
+        font-size: large;
+        padding: 0.75em;
+    }
+</style>
