1dc8a5cf5d95416cc0e5594236e977357f610253
Caleb Favela <cfc3.1415@gmail.com>
2022-07-17 21:57:14 -0500

    add service worker

--- sw.mjs
+++ sw.mjs
@@ -4,15 +4,16 @@ import { generateSW } from 'workbox-build';
 generateSW({
 	globDirectory: 'dist/',
 	globPatterns: ['**/*.{css,woff2,png,svg,jpg,js}'],
+  maximumFileSizeToCacheInBytes: 5000000000,
 	swDest: 'dist/sw.js',
   runtimeCaching: [{
     // Routing via a matchCallback function:
-    urlPattern: ({request, url}) => ...,
-    handler: 'CacheFirst',
+    urlPattern: ({ url }) => url.origin === self.location.origin && url.pathname.startsWith('/'),
+    handler: 'StaleWhileRevalidate',
     options: {
-      cacheName: '...',
+      cacheName: 'MainCache',
       expiration: {
-        maxEntries: ...,
+        maxEntries: 100,
       },
     },
   }, ],
