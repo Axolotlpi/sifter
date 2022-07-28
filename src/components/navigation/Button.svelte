e5d4e4c80a45a9fa8e55949892cb19437bfc052b
Caleb Favela <cfc3.1415@gmail.com>
2022-07-27 22:20:49 -0500

    create toggleable button component

---
+++ src/components/navigation/Button.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export let srText = '';
+    export let toggledState = false; //bindable
+    export let onClick = () => null; //bindable
+
+    const internalOnClick = () => {
+        toggledState = !toggledState;
+        onClick();
+    }
+</script>
+
+<button on:click={internalOnClick} type="button" class="inline-flex items-center justify-center p-2 rounded-md text-primary-dark-2 hover:text-primary-dark-1 hover:bg-primary-1 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-secondary-2" aria-controls="mobile-menu" aria-expanded="false">
+    {#if srText}
+        <span class="sr-only">{srText}</span>
+    {/if}
+    <slot name="left" />
+    <slot />
+    <slot name="right" />
+</button>
