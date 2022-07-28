b3541685de51854f10eaee96473c543d63683bc4
Caleb Favela <cfc3.1415@gmail.com>
2022-07-27 22:21:21 -0500

    create horizontal and vertical bars

---
+++ src/components/navigation/HorizontalBar.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    type Link = {
+        href: string;
+        text: string;
+    }
+    export let links: Array<Link>;
+    export let activeHref: string = '';
+</script>
+
+<div class="flex space-x-4">
+    {#each links as link}
+        {#if link.href == activeHref}
+            <a href={link.href} class="inline-flex items-center justify-center p-2 rounded-md text-primary-dark-1 bg-primary-1 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-secondary-2" aria-current="page">{link.text}</a>
+        {:else}
+            <a href={link.href} class="inline-flex items-center justify-center p-2 rounded-md text-primary-dark-2 hover:text-primary-dark-1 hover:bg-primary-1 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-secondary-2">{link.text}</a>
+        {/if}
+    {/each}
+</div>
---
+++ src/components/navigation/VerticalBar.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    type Link = {
+        href: string;
+        text: string;
+    }
+    export let links: Array<Link>;
+    export let activeHref: string = '';
+</script>
+
+<div class="px-2 pt-2 pb-3 space-y-1">
+    {#each links as link}
+        {#if link.href == activeHref}
+            <a href={link.href} class="bg-primary-1 text-primary-dark-1 block px-3 py-2 rounded-md text-base font-medium" aria-current="page">{link.text}</a>
+        {:else}
+            <a href={link.href} class="text-primary-dark-2 hover:bg-primary-1 hover:text-primary-dark-1 block px-3 py-2 rounded-md text-base font-medium">{link.text}</a>
+        {/if}
+    {/each}
+</div>
