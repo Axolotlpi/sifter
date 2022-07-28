53b9947d0b02aee4308e00bb5aac77f4b4a6e765
Caleb Favela <cfc3.1415@gmail.com>
2022-07-27 21:11:14 -0500

    create responsive logo component

---
+++ src/components/Logo.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let smallSrc;
+    export let largeSrc;
+    export let alt;
+</script>
+
+<img class="block lg:hidden h-8 w-auto" src={smallSrc} alt={alt}>
+<img class="hidden lg:block h-8 w-auto" src={largeSrc} alt={alt}>
