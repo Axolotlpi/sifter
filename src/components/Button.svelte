4af1007ca5c286599ef2a7dd6e7fa917bf326466
Caleb Favela <cfc3.1415@gmail.com>
2022-08-03 18:41:11 -0500

    cleanup before adding new page

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -9,7 +9,7 @@
     }
 </script>
 
-<button on:click={internalOnClick} type="button" class="h-min inline-flex items-center justify-center space-x-1 p-2 rounded-md text-primary-dark-2 hover:text-primary-dark-1 hover:bg-primary-1 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-secondary-2" aria-controls="mobile-menu" aria-expanded="false">
+<button on:click={internalOnClick} type="button" class="h-min inline-flex items-center justify-center space-x-1 p-2 rounded-md text-primary-dark-2 hover:text-primary-dark-1 hover:bg-primary-1 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-secondary-2" aria-expanded="false">
     {#if srText}
         <span class="sr-only">{srText}</span>
     {/if}
