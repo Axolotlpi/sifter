ea529931d538b1500200d2948a77e0a11fa490ad
Caleb Favela <cfc3.1415@gmail.com>
2022-08-15 22:32:14 -0500

    Add message and name to log viewer

--- src/components/LogViewer/LogViewer.svelte
+++ src/components/LogViewer/LogViewer.svelte
@@ -51,7 +51,7 @@
 {:else}
     {#await readLogPromise then logLines} 
         <div class="w-full flex justify-end">
-            <div class="absolute p-2 pr-4">
+            <div class="absolute p-2 mr-3">
                 <Button onClick={() => openFile = true}>
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -64,20 +64,26 @@
                 <Liner text={text} id={`logline-${lineNum}`} highlight={highlightedLine == lineNum ? [0, null] : []} highlightColor={highlightColor} {lineNum} lineBreak />
             {/each}
         </TextView>
-        <Tabs tabList={logLines.snippedLines.map(line => line.snippet.pattern)}>
+        <div class="p-2 rounded-lg bg-primary-0">
+        <Tabs tabList={logLines.snippedLines.map(line => line.snippet.name)}>
             <svelte:fragment let:currentTab={currentTab}>
                 <div class="min-h-[50vh]">
-                    {#each logLines.snippedLines as {snippet, lines} (snippet.pattern)}
-                        {#if lines.length > 0 && currentTab == snippet.pattern}
-                            <TextView>
-                                {#each lines as {text, lineNum, match} (lineNum)}
-                                    <Liner text={text} {lineNum} highlight={match} highlightColor={snippet.color} onClick={() => goToLine(lineNum)} lineBreak/>
-                                {/each}
-                            </TextView>
+                    {#each logLines.snippedLines as {snippet, lines} (snippet.name)}
+                        {#if lines.length > 0 && currentTab == snippet.name}
+                            <div>
+                                <TextView>
+                                    {#each lines as {text, lineNum, match} (lineNum)}
+                                        <Liner text={text} {lineNum} highlight={match} highlightColor={snippet.color} onClick={() => goToLine(lineNum)} lineBreak/>
+                                    {/each}
+                                </TextView>
+                                <p class="p-1 my-2 rounded bg-primary-dark-2 text-primary-1"><span class="font-bold text-secondary-1">Pattern: </span>"{snippet.pattern}"</p>
+                                <p class="rounded p-1 bg-primary-1 text-primary-dark-2">{snippet.message}</p>
+                            </div>
                         {/if}
                     {/each}
                 </div>
             </svelte:fragment>
         </Tabs>
+        </div>
     {/await}
 {/if}
