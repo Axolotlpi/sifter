cf37c8e837e65f6e1e2cc5295e643f4d26b61015
Caleb Favela <cfc3.1415@gmail.com>
2022-09-12 20:22:09 -0500

    cache almost everything

--- workbox.config.js
+++ workbox.config.js
@@ -1,12 +1,12 @@
 export const workboxConfig = {
 	globDirectory: 'dist/',
-	globPatterns: ['**/*.{css,woff2,png,svg,jpg,js}'],
+	globPatterns: ['**/*.{css,woff2,png,svg,jpg,js,html,webmanifest,ico}'],
 	maximumFileSizeToCacheInBytes: 5000000000,
 	swDest: 'dist/sw.js',
 	runtimeCaching: [
 		{
 			// Routing via a matchCallback function:
-			urlPattern: ({ url }) => url.origin === self.location.origin && url.pathname.startsWith('/'),
+			urlPattern: ({ url }) => url.origin === self.location.origin,
 			handler: 'NetworkFirst',
 			options: {
 				cacheName: 'MainCache',
