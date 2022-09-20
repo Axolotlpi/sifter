bd56b5c5e430b7ce6404edff75a6d443370c8afe
Caleb Favela <cfc3.1415@gmail.com>
2022-09-20 17:42:01 -0500

    cleanup info

--- src/pages/Info.svelte
+++ src/pages/Info.svelte
@@ -11,7 +11,7 @@
 	logoSmall={params.logo}
 />
 <main>
-	<div class="w-full p-4 lg:px-8 pt-0 font-monospace">
+	<div class="w-full p-4 lg:px-10 pt-0 font-monospace text-xl">
 		<h1>About Sifter</h1>
 		<p>
 			Sifter is a project of hate, like any other. Reading words is hard, reading a lot of them is
@@ -21,18 +21,18 @@
 			Ironically enough. it is. in itself. an ill designed project.
 		</p>
 		<h2 class="h3">Features:</h2>
-		<ul>
+		<ul class="m-4 p-2 rounded bg-secondary-dark-0 text-primary-0">
 			<li>
-				Custom saved searches - search the same regex patterns over and over, and you get to define
-				exactly what thesepatterns are
+				<span class="font-bold">Custom saved searches</span> - search the same regex patterns over and
+				over, and you get to define exactly what thesepatterns are
 			</li>
 			<li>
-				No cloud dependance - all of your configuration is saved in json and in the browser. You can
-				download it and save it and none of it is ever sent to any backend.
+				<span class="font-bold">No cloud dependance</span> - all of your configuration is saved in json
+				and in the browser. You can download it and save it and none of it is ever sent to any backend.
 			</li>
 			<li>
-				Minimalistic Design - I just need some other point to make 3 points. But seriously, it’s not
-				half bad
+				<span class="font-bold">Works completely offline</span> - yes, for the rare occacion that you
+				need to read a log while disconnected from the internet.
 			</li>
 		</ul>
 
@@ -52,28 +52,110 @@
 			separate editor that can confirm the syntax for the time being. Aside from plain JSON syntax
 			rules here are the guidelines when editing your configuration:
 		</p>
-		<ul>
-			<li>
-				id - this is an automatically generated unique identifier, and should never be modified
-			</li>
-			type - this is also automatic and defines the type of configuration and changing this should also
-			be avoided
-			<li>name - this is the name of the profile and up to the user. Recommended to be short.</li>
-			<li>
-				snippets - this is a list containing objects with the following format: (don’t forget to put
-				commas between each object)
-			</li>
-			<li>name - short name to define the snippet, this will be shown for each snippet’s tab.</li>
-			<li>
-				pattern - this is the regex pattern used to match the text input. No flags are currently
-				allowed.
-			</li>
-			<li>message - this is simply for user’s notes and is shown underneath the snippet results</li>
-			<li>
-				color - this is a css color value (eg. hex, rgb) used to highlight the search text, for
-				recognizability.
-			</li>
-		</ul>
+		<div class="overflow-x-auto relative">
+			<table class="w-full text-sm text-left text-primary-2">
+				<thead class="text-xs text-primary-1 uppercase bg-gray-50 dark:bg-gray-700">
+					<tr>
+						<th scope="col" class="py-3 px-6"> Feild </th>
+						<th scope="col" class="py-3 px-6"> Description </th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr class="bg-primary-0 border-b dark:bg-gray-800 dark:border-gray-700">
+						<th
+							scope="row"
+							class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+						>
+							id
+						</th>
+						<td class="py-4 px-6">
+							this is an automatically generated unique identifier, and should never be modified
+						</td>
+					</tr>
+					<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+						<th
+							scope="row"
+							class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+						>
+							type
+						</th>
+						<td class="py-4 px-6">
+							this is also automatic and defines the type of configuration and changing this should
+							also be avoided
+						</td>
+					</tr>
+					<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+						<th
+							scope="row"
+							class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+						>
+							name
+						</th>
+						<td class="py-4 px-6">
+							this is the name of the profile and up to the user. Recommended to be short.
+						</td>
+					</tr>
+					<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+						<th
+							scope="row"
+							class="underline py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+						>
+							snippets
+						</th>
+						<td class="py-4 px-6">
+							this is a list containing objects with the following format: (don’t forget to put
+							commas between each object)
+						</td>
+					</tr>
+					<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+						<th
+							scope="row"
+							class="py-4 px-6 pl-10 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+						>
+							name
+						</th>
+						<td class="py-4 px-6">
+							short name to define the snippet, this will be shown for each snippet’s tab.
+						</td>
+					</tr>
+					<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+						<th
+							scope="row"
+							class="py-4 px-6 pl-10 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+						>
+							pattern
+						</th>
+						<td class="py-4 px-6">
+							this is the regex pattern used to match the text input. No flags are currently
+							allowed.
+						</td>
+					</tr>
+					<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+						<th
+							scope="row"
+							class="py-4 px-6 pl-10 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+						>
+							message
+						</th>
+						<td class="py-4 px-6">
+							this is simply for user’s notes and is shown underneath the snippet results
+						</td>
+					</tr>
+					<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+						<th
+							scope="row"
+							class="py-4 px-6 pl-10 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+						>
+							color
+						</th>
+						<td class="py-4 px-6">
+							this is a css color value (eg. hex, rgb) used to highlight the search text, for
+							recognizability.
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
 	</div>
 </main>
 
