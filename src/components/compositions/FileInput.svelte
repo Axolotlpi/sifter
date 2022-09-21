c9627139369746954ec23a95c62f98eefae22af2
Caleb Favela <cfc3.1415@gmail.com>
2022-09-21 18:26:51 -0500

    fix loader style

--- src/components/compositions/FileInput.svelte
+++ src/components/compositions/FileInput.svelte
@@ -113,10 +113,10 @@
 		{#await fileOpenPromise}
 			<div class="flex justify-center items-center">
 				<div
-					class="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
+					class="spinner-border animate-spin inline-block w-8 h-8 border-4 border-t-secondary-dark-0 rounded-full"
 					role="status"
 				>
-					<span class="visually-hidden">Loading...</span>
+					<span class="hidden">Loading...</span>
 				</div>
 			</div>
 		{:then file}
