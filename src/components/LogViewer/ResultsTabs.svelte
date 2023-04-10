1ae8245c808941efa319f0c698c8faf529680fdf
Caleb Favela <cfc3.1415@gmail.com>
2023-04-10 17:07:03 +0000

    make result messages available despite no result

--- src/components/LogViewer/ResultsTabs.svelte
+++ src/components/LogViewer/ResultsTabs.svelte
@@ -49,32 +49,32 @@
 		<svelte:fragment let:currentTab>
 			<div class="min-h-[50vh]">
 				{#each parsedLines.snippedLines as { snippet, lines } (snippet.name)}
-					{#if lines.length > 0 && currentTab == snippet.name}
-						<div>
-							<div class="h-[60vh]">
-								<TextView>
-									{#each lines as { text, lineNum, match }, index (lineNum)}
-										<Liner
-											{text}
-											lineNum={lineNum + ''}
-											highlight={match}
-											highlightColor={index == currentResult ? $selectedProfile.highlightColor : snippet.color}
-											onClick={() => selectResult(index)}
-											lineBreak
-										/>
-									{/each}
-								</TextView>
+						{#if currentTab == snippet.name}	
+							<div class="h-[60vh] flex flex-col">
+								{#if lines.length > 0}
+									<TextView>
+										{#each lines as { text, lineNum, match }, index (lineNum)}
+											<Liner
+												{text}
+												lineNum={lineNum + ''}
+												highlight={match}
+												highlightColor={index == currentResult ? $selectedProfile.highlightColor : snippet.color}
+												onClick={() => selectResult(index)}
+												lineBreak
+											/>
+										{/each}
+									</TextView>
+								{/if}
+								{#if snippet.pattern}
+									<p class="p-1 my-2 rounded bg-primary-dark-2 text-primary-1">
+										<span class="font-bold text-secondary-1">Pattern: </span>"{snippet.pattern}"
+									</p>
+								{/if}
+								{#if snippet.message}
+									<p class="rounded p-1 bg-primary-1 text-primary-dark-2">{snippet.message}</p>
+								{/if}
 							</div>
-							{#if snippet.pattern}
-								<p class="p-1 my-2 rounded bg-primary-dark-2 text-primary-1">
-									<span class="font-bold text-secondary-1">Pattern: </span>"{snippet.pattern}"
-								</p>
-							{/if}
-							{#if snippet.message}
-								<p class="rounded p-1 bg-primary-1 text-primary-dark-2">{snippet.message}</p>
-							{/if}
-						</div>
-					{/if}
+						{/if}
 				{/each}
 			</div>
 		</svelte:fragment>
